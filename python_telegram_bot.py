426245e8582bbff3d7b67046edf43b94509863dd
nakompuktere <144929164+nakompuktere@users.noreply.github.com>
2024-12-07 14:49:38 +0300

    Убрал argparse и цикл while

--- python_telegram_bot.py
+++ python_telegram_bot.py
@@ -7,28 +7,22 @@ import random
 from download_comic_images import get_comic_image
 
 
-def send_files(delay, chat_id, bot):
+def send_files(chat_id, bot):
     first_comic_id = 1
     last_comic_id = 614
-    while True:
-        random_number = random.randint(first_comic_id, last_comic_id)
-        get_comic_image(random_number)
+    random_number = random.randint(first_comic_id, last_comic_id)
+    get_comic_image(random_number)
+    with open("images/comic.png", 'rb') as file:
+        bot.send_document(chat_id=chat_id, document=file)
 
-        with open("images/comic.png", 'rb') as file:
-            bot.send_document(chat_id=chat_id, document=file)
-        time.sleep(delay)
 
 
 def main():
     load_dotenv()
     bot = telegram.Bot(token=os.getenv("TELEGRAM_TOKEN"))
     default_chat_id = os.getenv("TG_CHAT_ID")
-    parser = argparse.ArgumentParser(description='бот присылает комикс один раз в день')
-    parser.add_argument('--chat_id', help='ваш chat id', default=default_chat_id)
-    parser.add_argument('--delay', help='выберете задержку', default=86400, type=int)
-    args = parser.parse_args()
     try:
-        send_files(args.delay, args.chat_id, bot)
+        send_files(default_chat_id, bot)
     finally:
         os.remove("images/comic.png")
 
