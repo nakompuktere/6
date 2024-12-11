baf73823b99d266a32844f8d5d34eae4d8aa7d73
nakompuktere <144929164+nakompuktere@users.noreply.github.com>
2024-12-11 17:31:26 +0300

    Разбил функцию на части

--- send_image.py
+++ send_image.py
@@ -8,21 +8,24 @@ from download_comic_images import get_comic_image
 
 
 def send_files(chat_id, bot, image_path):
-    first_comic_id = 1
-    last_comic_id = 614
-    random_number = random.randint(first_comic_id, last_comic_id)
-    get_comic_image(random_number)
     with open(image_path, 'rb') as file:
         bot.send_document(chat_id=chat_id, document=file)
 
 
 
 def main():
-    image_path = os.path.join("images", "comic.png")
     Path("images").mkdir(parents=True, exist_ok=True)
     load_dotenv()
+    image_path = os.path.join("images", "comic.png")
     bot = telegram.Bot(token=os.environ["TELEGRAM_TOKEN"])
     default_chat_id = os.environ["TG_CHAT_ID"]
+
+    first_comic_id = 1
+    last_comic_id = 614
+    random_number = random.randint(first_comic_id, last_comic_id)
+    get_comic_image(random_number)
+
+
     try:
         send_files(default_chat_id, bot, image_path)
     finally:
